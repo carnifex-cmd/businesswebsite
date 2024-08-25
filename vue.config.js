5246b797a3cb7e6cde3d5a354c3ae4802888de21
Shardul Sawant <shardulsawant@Sharduls-MacBook-Pro.local>
2024-08-26 00:29:40 +0530

    added allowdHosts

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  disableHostCheck: true
+  devServer: {
+    allowedHosts: 'all', // This allows all hosts, similar to disableHostCheck
+  },
 })
